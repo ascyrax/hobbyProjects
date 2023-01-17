bcb84036945e73c55bad5b56cff8a3c75bb875a1
ascyrax <ascyrax01@gmail.com>
2023-01-17 18:19:10 +0530

    register page

--- chat app mern socketio/client/src/components/Register.jsx
+++ chat app mern socketio/client/src/components/Register.jsx
@@ -1,21 +1,87 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faEnvelope, faKey } from "@fortawesome/free-solid-svg-icons";
+import {
+	faFacebook,
+	faTwitter,
+	faGoogle,
+	faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 export default function Register() {
 	return (
 		<div className="register">
-			<h1>Register here</h1>
-			<form className="form-register">
-				<input
-					type="text"
-					name="username"
-					id="username"
-					placeholder="username"
-				/>
-				<input type="text" name="email" id="email" />
-				<input type="text" name="password" id="password" />
-				<input type="text" name="password2" id="password2" />
-				<button type="submit">register</button>
-			</form>
+			<div className="left">
+				<h1>Sign up</h1>
+				<form className="form-register">
+					<div className="input-field">
+						<FontAwesomeIcon icon={faUser} />
+						<input
+							type="text"
+							name="username"
+							id="username"
+							placeholder="Username"
+							className="input"
+						/>
+					</div>
+					<div className="input-field">
+						<FontAwesomeIcon icon={faEnvelope} />
+						<input
+							type="text"
+							name="email"
+							id="email"
+							placeholder="Email"
+							className="input"
+						/>
+					</div>
+					<div className="input-field">
+						<FontAwesomeIcon icon={faKey} />
+						<input
+							type="text"
+							name="password"
+							id="password"
+							placeholder="Password"
+							className="input"
+						/>
+					</div>
+					<div className="input-field">
+						<FontAwesomeIcon icon={faKey} />
+						<input
+							type="text"
+							name="password2"
+							id="password2"
+							placeholder="Password"
+							className="input"
+						/>
+					</div>
+					<button type="submit" className="btn-register">
+						REGISTER
+					</button>
+				</form>
+				<div className="div-links">
+					<p className="mssg">Or Register with social platforms</p>
+					<div className="links">
+						<a href="#" className="link fb">
+							<FontAwesomeIcon icon={faFacebook} />
+						</a>
+						<a href="#" className="link twitter">
+							<FontAwesomeIcon icon={faTwitter} />
+						</a>
+						<a href="#" className="link google">
+							<FontAwesomeIcon icon={faGoogle} />
+						</a>
+						<a href="#" className="link linkedin">
+							<FontAwesomeIcon icon={faLinkedin} />
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="right">
+				<div className="right-top">
+					<h1>Already Registered?</h1>
+					<button className="btn-signIn">SIGN IN</button>
+				</div>
+				<div className="right-bottom"></div>
+			</div>
 		</div>
 	);
 }
